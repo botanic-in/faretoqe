d0f043abc2416c0bfa6ef2464626866087a4ad61
tomomichi <tomomichi.onishi@gmail.com>
2017-03-11 00:18:00 +0900

    fix sp menu and calendar

--- js/application.js
+++ js/application.js
@@ -5,14 +5,10 @@ $(function(){
     $('#menu').toggleClass('fixed', $(this).scrollTop() > offset);
   });
 
-  // カレンダーBOXの高さ調整
-  // （PC版用。SP版はアコーディオン表示時に再実行）
-  adjustCalendar();
-
   // SP版アコーディオン表示
   var isVisibleAccordion = false;
   $('.btn-accordion').on('click', function(){
-    $('.accordion').animate({height: 'toggle'}, setTimeout(function(){adjustCalendar()}, 500));
+    $('.accordion').animate({height: 'toggle'}, 'slow');
 
     // ボタンテキストの変更
     if(isVisibleAccordion==true){
@@ -23,12 +19,6 @@ $(function(){
       isVisibleAccordion = true;
     }
   });
-
-  // カレンダーBOXの高さ調整
-  function adjustCalendar() {
-    var cal_box_width = $('#calendar .box').width();
-    $('#calendar .box').height(cal_box_width);
-  }
 
   // SP版メニュー表示
   $('#btn-sp-menu').on('click', function(){
