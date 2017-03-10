48ce9eea02019900b9de7d5e9d3b934039a71351
tomomichi <tomomichi.onishi@gmail.com>
2017-03-10 22:17:50 +0900

    add accordion menu

--- js/application.js
+++ js/application.js
@@ -6,6 +6,27 @@ $(function(){
   });
 
   // カレンダーBOXの高さ調整
-  var cal_box_width = $('#calendar .box').width();
-  $('#calendar .box').height(cal_box_width);
+  // （PC版用。SP版はアコーディオン表示時に再実行）
+  adjustCalendar();
+
+  // SP版アコーディオン表示
+  var isVisibleAccordion = false;
+  $('.btn-accordion').on('click', function(){
+    $('.accordion').toggle('slow', '', setTimeout(function(){adjustCalendar()}, 500));
+
+    // ボタンテキストの変更
+    if(isVisibleAccordion==true){
+      $(this).text('さらに詳しく ▼');
+      isVisibleAccordion = false;
+    }else {
+      $(this).text('閉じる ▼');
+      isVisibleAccordion = true;
+    }
+  });
+
+  // カレンダーBOXの高さ調整
+  function adjustCalendar() {
+    var cal_box_width = $('#calendar .box').width();
+    $('#calendar .box').height(cal_box_width);
+  }
 });
