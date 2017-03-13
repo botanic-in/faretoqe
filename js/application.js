03ab69865897d5d3902169ce852fa764c11fa898
tomomichi <tomomichi.onishi@gmail.com>
2017-03-13 22:19:39 +0900

    fix in-page scroll

--- js/application.js
+++ js/application.js
@@ -1,4 +1,16 @@
 $(function(){
+  // ページ内スクロール
+  // var headerHight = 140; //ヘッダの高さ
+  var headerHeight = $('#menu').height(); //ヘッダー高さ（SP版では0）
+  console.log(headerHeight);
+  $('a[href^=\\#]').click(function(){
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top-headerHeight; //ヘッダの高さ分位置をずらす
+      $("html, body").animate({scrollTop:position}, 550, "swing");
+      return false;
+  });
+
   // PC版メニューの固定表示
   var offset = $('#menu').offset().top;
   $(window).on('scroll', function() {
